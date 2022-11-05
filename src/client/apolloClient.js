132368d437aafea051c8dafb05d3c9686e1e7dcc
Moahmedahmadian <42762811+Moahmedahmadian@users.noreply.github.com>
2022-11-05 12:49:51 +0330

    optimize client structure

---
+++ src/client/apolloClient.js
@@ -0,0 +1,45 @@
+// we should import like this.. changing will throw error 
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core/core.cjs';
+import { HttpLink } from "@apollo/client/link/http/http.cjs";
+import fetch from 'cross-fetch';
+
+class ApolloClientService {
+    client = null;
+    query = ''
+    createClient() {
+        this.client = new ApolloClient({
+            link: new HttpLink({ uri: 'http://localhost:4000/graphql', fetch }),
+            cache: new InMemoryCache()
+        })
+    }
+    async runQuery() {
+        let result = await this.client.query({
+            query: gql(this.query)
+        })
+
+        console.log("-----------------------------");
+        console.log(new Date().toLocaleTimeString() + ' result:');
+        console.log(result.data);
+        console.log("-----------------------------");
+    }
+
+
+}
+
+async function run() {
+    const service = new ApolloClientService()
+    service.createClient()
+    service.query = `
+    query{
+        users {
+                userId
+              }
+        }
+         `
+    service.runQuery()
+}
+
+run()
+
+
+
